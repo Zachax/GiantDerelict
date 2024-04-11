3e7aea4797fb90acc7d135758fbb553d5585ed1c
Sakari Eskelinen <35963527+Zachax@users.noreply.github.com>
2024-04-11 22:19:00 +0300

    Update giantderelict.js

--- giantderelict.js
+++ giantderelict.js
@@ -1,12 +1,14 @@
 // script.js
 alert('This is JavaScript from an external file running on GitHub Pages!');
 
-const app = new PIXI.Application({ width: 800, height: 600 });
-document.body.appendChild(app.view);
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new PIXI.Application({ width: 800, height: 600, backgroundColor: 0x1099bb });
+    document.getElementById('pixi-container').appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
-graphics.beginFill(0xFF0000);
-graphics.drawRect(0, 0, 100, 100);
-graphics.endFill();
+    const graphics = new PIXI.Graphics();
+    graphics.beginFill(0xFF0000);
+    graphics.drawRect(0, 0, 100, 100);
+    graphics.endFill();
 
-app.stage.addChild(graphics);
+    app.stage.addChild(graphics);
+});
