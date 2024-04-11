fd1d46cd2ba34fa81f91d1df2b256634f4aaed5a
Sakari Eskelinen <35963527+Zachax@users.noreply.github.com>
2024-04-11 22:15:26 +0300

    Update giantderelict.js
    Added PixiJS test

--- giantderelict.js
+++ giantderelict.js
@@ -1,2 +1,12 @@
 // script.js
 alert('This is JavaScript from an external file running on GitHub Pages!');
+
+const app = new PIXI.Application({ width: 800, height: 600 });
+document.body.appendChild(app.view);
+
+const graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0000);
+graphics.drawRect(0, 0, 100, 100);
+graphics.endFill();
+
+app.stage.addChild(graphics);
